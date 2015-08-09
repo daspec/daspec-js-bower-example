26ebfe45745887e04168597efa07cef5b5eae7fa
Gojko Adzic <gojko@gojko.com>
2015-08-09 17:24:59 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+Example [DaSpec JS](http://daspec.com) setup running inside a browser, with components managed using Bower, for testing javascript projects.
+
+Check out the git repository, then do 
+
+    bower install
+
+to pull the dependencies. You should be able to open the [index.html](index.html) page and execute DaSpec specifications in your browser.
+
+## How this works
+
+The magic is in [runspec.js](runspec.js) - that file shows how to wire up DaSpec with a minimal configuration that outputs markdown and produces a
+counter for passed, failed and skipped steps. 
+
+
+For more information on DaSpec, see [http://daspec.com](http://daspec.com)
---
+++ runspec.js
@@ -0,0 +1,32 @@
+/*global DaSpec, window */
+var runDaSpec = function (spec, steps, systemUnderTest) {
+	'use strict';
+	var exportObject = function (object) {
+			var v;
+			for (v in object) {
+				window[v] = object[v];
+			}
+		},
+		defineSteps = function (context) {
+			exportObject(context); // syntax shugar, allow steps to just call defineStep instead of context.defineStep
+			eval(systemUnderTest + '\n' + steps);
+		},
+		runner, result, markdownFormatter, counter, compositeFormatter;
+	try {
+		markdownFormatter = new DaSpec.MarkdownResultFormatter();
+		counter = new DaSpec.CountingResultFormatter();
+		compositeFormatter = new DaSpec.CompositeResultFormatter();
+		compositeFormatter.add(markdownFormatter);
+		compositeFormatter.add(counter);
+		runner = new DaSpec.Runner(defineSteps, compositeFormatter);
+		runner.example(spec);
+		result = markdownFormatter.formattedResults();
+	} catch (e) {
+		result = '    ' + (e.stack || e.message || e.name || 'there was a problem executing the specification');
+	}
+	return {
+		text: result,
+			counts:  counter.current
+	};
+};
+
